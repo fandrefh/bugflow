171a0d27d4354f002f77760166837d38efc0b28d
Francisco André <fandrefh@gmail.com>
2018-08-25 17:48:54 -0300

    Alteração da senha

--- bugflow/accounts/urls.py
+++ bugflow/accounts/urls.py
@@ -7,6 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('novo/', views.add_user, name='add-user'),
     path('editar/', views.edit_user, name='edit-user'),
+    path('trocar-senha/', views.change_password, name='change-password'),
     path('login/', views.user_login, name='user-login'),
     path('logout/', views.user_logout, name='user-logout'),
     path('profile/', views.user_profile, name='user-profile'),
--- bugflow/accounts/views.py
+++ bugflow/accounts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import ( 
+    authenticate, login, logout,
+    update_session_auth_hash
+)
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .forms import UserForm, EditUserForm
 
@@ -52,7 +56,21 @@ def user_logout(request):
     logout(request)
     return redirect('/')
 
-
 def user_profile(request):
     user = User.objects.get(username=request.user)
     return render(request, 'accounts/user_profile.html', {'user': user})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Senha alterada com sucesso.')
+            return redirect('accounts:user-profile')
+        else:
+            messages.error(request, 'Alteração não realizada.')
+            
+    form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {'form': form})
